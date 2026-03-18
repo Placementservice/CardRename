99205619c4faf698934646516bac70147dafb94b
Placementservice <services@gorservices.ru>
2026-03-18 17:14:31 +0300

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -70,14 +70,59 @@ Addon.initialize({
                     // Присваиваем переменной значение name
                     let typeNameField = cardTypeName;
 
+                    // Функция для определения 5-й части названия на основе типа работ
+                    const getWorkTypeAbbreviation = (workType) => {
+                        switch(workType) {
+                            case 'Модернизация':
+                                return 'Мод';
+                            case 'ВОЛС':
+                                return 'ВОЛС';
+                            case 'Новая стройка':
+                                return 'НС';
+                            default:
+                                return workType; // Если тип не определен, возвращаем исходное значение
+                        }
+                    };
+
+                    // Получаем сокращение для типа работ
+                    const workTypeAbbr = getWorkTypeAbbreviation(workTypeField);
+
+                    // Формируем название карточки
+                    let cardName = '';
+
+                    // Часть 1: bsNumberField
+                    cardName += bsNumberField;
+                    
+                    // Часть 2: sublessorField
+                    cardName += '_' + sublessorField;
+                    
+                    // Часть 3: objectField
+                    cardName += '_' + objectField;
+                    
+                    // Часть 4: typeNameField
+                    cardName += '_' + typeNameField;
+                    
+                    // Часть 5: workTypeAbbr (сокращение типа работ)
+                    cardName += '_' + workTypeAbbr;
+                    
+                    // Часть 6: если correctionTagExists = true, добавляем '_корр'
+                    if (correctionTagExists) {
+                        cardName += '_корр';
+                    }
+
+                    // Присваиваем переменной сформированное название карточки
+                    let cardTitle = cardName;
+
                     // Выводим все переменные с их названиями и значениями в консоль
                     console.log('=== РЕЗУЛЬТАТЫ ===');
                     console.log('correctionTagExists (Наличие метки "Корректировка ТУ"):', correctionTagExists);
                     console.log('objectField (Объект):', objectField);
                     console.log('sublessorField (Субарендатор (Оператор)):', sublessorField);
                     console.log('workTypeField (Тип работ):', workTypeField);
+                    console.log('workTypeAbbr (Сокращение типа работ):', workTypeAbbr);
                     console.log('bsNumberField (Номер БС):', bsNumberField);
                     console.log('typeNameField (Тип карточки - name):', typeNameField);
+                    console.log('cardTitle (Сформированное название карточки):', cardTitle);
                     console.log('=== КОНЕЦ РЕЗУЛЬТАТОВ ===');
 
                     // Можно использовать переменные для дальнейшей логики
