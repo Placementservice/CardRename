42f83c5cb06fd459375b30f13f84401b59e9fcc2
Placementservice <services@gorservices.ru>
2026-03-18 16:54:49 +0300

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -15,8 +15,6 @@ Addon.initialize({
                     
                     // Присваиваем результат переменной
                     let correctionTagExists = hasCorrectionTag;
-                    
-                    console.log('Результат проверки наличия метки "Корректировка ТУ":', correctionTagExists);
 
                     // Получаем пользовательские свойства (custom fields)
                     const customProps = await callbackContext.getCardProperties('customProperties');
@@ -57,28 +55,42 @@ Addon.initialize({
                     let workTypeField = workTypeValue;
                     let bsNumberField = bsNumberValue;
 
-                    // Выводим полученные значения в консоль для проверки
-                    console.log('Объект:', objectField);
-                    console.log('Субарендатор (Оператор):', sublessorField);
-                    console.log('Тип работ:', workTypeField);
-                    console.log('Номер БС:', bsNumberField);
+                    // Получаем значение name из типа карточки
+                    const cardType = await callbackContext.getCardProperties('type');
+                    console.log('Тип карточки:', cardType);
+                    
+                    // Извлекаем name из типа карточки (если это объект с полем name)
+                    let cardTypeName = 'XXXX';
+                    if (cardType && typeof cardType === 'object' && cardType.name) {
+                        cardTypeName = cardType.name;
+                    } else if (typeof cardType === 'string') {
+                        cardTypeName = cardType;
+                    }
+
+                    // Присваиваем переменной значение name
+                    let typeNameField = cardTypeName;
+
+                    // Выводим все переменные с их названиями и значениями в консоль
+                    console.log('=== РЕЗУЛЬТАТЫ ===');
+                    console.log('correctionTagExists (Наличие метки "Корректировка ТУ"):', correctionTagExists);
+                    console.log('objectField (Объект):', objectField);
+                    console.log('sublessorField (Субарендатор (Оператор)):', sublessorField);
+                    console.log('workTypeField (Тип работ):', workTypeField);
+                    console.log('bsNumberField (Номер БС):', bsNumberField);
+                    console.log('typeNameField (Тип карточки - name):', typeNameField);
+                    console.log('=== КОНЕЦ РЕЗУЛЬТАТОВ ===');
 
                     // Можно использовать переменные для дальнейшей логики
                     if (correctionTagExists) {
                         console.log('Метка "Корректировка ТУ" найдена');
                         // Здесь можно использовать полученные поля
                         console.log('Данные карточки с меткой:',
-                            objectField, sublessorField, workTypeField, bsNumberField
+                            objectField, sublessorField, workTypeField, bsNumberField, typeNameField
                         );
                     } else {
                         console.log('Метка "Корректировка ТУ" не найдена');
                         // Здесь можно выполнить другие действия
                     }
-
-                    // Получаем тип карточки (например, "Задача", "Дефект")
-                    const cardType = await callbackContext.getCardProperties('type');
-                    console.log('Тип карточки:', cardType);
-                    // --- КОНЕЦ НОВОГО КОДА ---
 
                 } catch (err) {
                     console.log('Ошибка при получении данных карточки:', err);
